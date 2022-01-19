725822ec6d27b4b6648319c183f89887f31f8ff6
Frazonar <snip.coloured@gmail.com>
2022-01-19 16:39:59 +0200

    feat: add support for Python generation

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -1,4 +1,6 @@
 const brainfuck = require('../index');
+const {PythonShell} = require('python-shell');
+const should = require('should');
 
 const helloWorldCode = '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.';
 const invalidCode = '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.';
@@ -28,6 +30,30 @@ describe('Transpilers tests', () => {
       });
       it('Returns cells array', () => {
         expect(Array.isArray(helloWorld().cells)).toBeTruthy();
+      });
+    });
+  });
+
+  describe('transpileToPython tests', () => {
+    const outputCode = brainfuck.transpileToPython(helloWorldCode);
+
+    it('Generates valid Python code', (done) => {
+      PythonShell.runString(outputCode, null, (err) => {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
+    });
+    describe('Generated Python code', () => {
+      it('Generates valid Python code', (done) => {
+        PythonShell.runString(outputCode, null, (err, results) => {
+          if (err) return done(err);
+          results.should.be.an.Array().and.have.lengthOf(1);
+          results.should.eql(['Hello World!']);
+          done();
+        });
       });
     });
   });
