de7929b9eb040d4d31325ad1da5074f262079c50
Frazonar <snip.coloured@gmail.com>
2022-01-20 20:36:56 +0200

    fix: fix c++ generation

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -82,13 +82,46 @@ describe('Transpilers tests', () => {
         const commandToRun = process.platform === 'win32' ? executableFile : `./${executableFile}`;
         exec(commandToRun)
           .then(({stdout, stderr}) => {
-            console.log(stdout.trim());
             if (stdout.trim() === 'Hello World!') {
               done();
             }
           })
           .catch((err) => {
-            console.log(err.message);
+            done(err);
+          });
+      });
+    });
+
+    afterAll(() => {
+      return Promise.all([fs.unlink(sourceFile), fs.unlink(executableFile)]);
+    });
+  });
+
+  describe('transpileToCpp tests', () => {
+    const outputCode = brainfuck.transpileToCpp(helloWorldCode);
+    const exeExtension = process.platform === 'win32' ? '.exe' : '';
+    const executableFile = `test${exeExtension}`;
+    const sourceFile = 'test.cpp';
+
+    beforeAll(() => {
+      return fs.writeFile(sourceFile, outputCode);
+    });
+
+    it('Generates valid C++ code', () => {
+      return expect(cppUtils.compileWithGPlus(sourceFile, executableFile, true)).resolves.toBeDefined();
+    });
+
+    describe('Generated C++ code', () => {
+      it('Has correct output', (done) => {
+        const commandToRun = process.platform === 'win32' ? executableFile : `./${executableFile}`;
+
+        exec(commandToRun)
+          .then(({stdout, stderr}) => {
+            if (stdout.trim() === 'Hello World!') {
+              done();
+            }
+          })
+          .catch((err) => {
             done(err);
           });
       });
