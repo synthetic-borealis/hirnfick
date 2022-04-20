adde3033f1b8acc87189edf533fe68cad2e6f175
Frazonar <snip.coloured@gmail.com>
2022-04-20 19:38:33 +0300

    test: rewrite C++ transpiler tests

--- __tests__/test.transpilers.js
+++ __tests__/test.transpilers.js
@@ -134,8 +134,45 @@ describe('Transpilers tests', () => {
     });
   });
 
-  describe('transpileToCpp tests', () => {
-    const outputCode = hirnfick.transpileToCpp(helloWorldCode);
+  describe('transpileToCpp tests (dynamic memory)', () => {
+    const outputCode = hirnfick.transpileToCpp(helloWorldCode, true);
+    const exeExtension = process.platform === 'win32' ? '.exe' : '';
+    const executableFile = `test${exeExtension}`;
+    const sourceFile = 'test.cpp';
+
+    beforeAll(() => {
+      return fs.writeFile(sourceFile, outputCode);
+    });
+
+    it('Generates valid C++ code', () => {
+      return expect(cppUtils.compileWithGPlus(sourceFile, executableFile, true)).resolves.toBeDefined();
+    });
+
+    describe('Generated C++ code', () => {
+      it('Has correct output', (done) => {
+        const commandToRun = process.platform === 'win32' ? executableFile : `./${executableFile}`;
+
+        exec(commandToRun)
+          .then(({stdout, stderr}) => {
+            if (stdout.trim() === 'Hello World!') {
+              done();
+            } else {
+              done(new Error('Incorrect output'));
+            }
+          })
+          .catch((err) => {
+            done(err);
+          });
+      });
+    });
+
+    afterAll(() => {
+      return Promise.all([fs.unlink(sourceFile), fs.unlink(executableFile)]);
+    });
+  });
+
+  describe('transpileToCpp tests (fixed memory)', () => {
+    const outputCode = hirnfick.transpileToCpp(helloWorldCode, false);
     const exeExtension = process.platform === 'win32' ? '.exe' : '';
     const executableFile = `test${exeExtension}`;
     const sourceFile = 'test.cpp';
