978cd33682d19a9ca1df58b251bcb777ea5f83b1
Frazonar <snip.coloured@gmail.com>
2022-08-27 21:44:50 +0300

    test: write pascal tests

--- __tests__/test.transpilers-pascal.js
+++ __tests__/test.transpilers-pascal.js
@@ -2,35 +2,49 @@ const pascalUtils = require('pascal-utils');
 const fs = require('fs/promises');
 const process = require('node:process');
 const util = require('util');
-const hirnfick = require('../index');
+const {
+  WrongInputTypeError,
+  BracketMismatchError,
+  transpileToPascal,
+} = require('../index');
 const exec = util.promisify(require('child_process').exec);
 
 const helloWorldCode = '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.';
+const bracketMismatchCode = '>>+++[[<-->]';
 
-describe('Transpilers tests', () => {
-  describe('transpileToPascal tests', () => {
-    const outputCode = hirnfick.transpileToPascal(helloWorldCode);
+describe('Pascal transpiler', () => {
+  describe('Error handling', () => {
+    it('Throws WrongInputTypeError when given input of wrong type', () => {
+      const numberArray = [2, 4, 8, 16];
+      // noinspection JSCheckFunctionSignatures
+      expect(() => transpileToPascal(numberArray)).toThrow(WrongInputTypeError);
+    });
+    it('Throws BracketMismatchError when there\'s a bracket mismatch', () => {
+      expect(() => transpileToPascal(bracketMismatchCode)).toThrow(BracketMismatchError);
+    });
+  });
+  describe('Code generation', () => {
+    const outputCode = transpileToPascal(helloWorldCode, 'Test');
     const exeExtension = process.platform === 'win32' ? '.exe' : '';
     const executableFile = `test${exeExtension}`;
     const sourceFile = 'test.pas';
-
+    const objectFile = 'test.o';
     beforeAll(() => fs.writeFile(sourceFile, outputCode));
-
-    it('Generates valid Pascal code', async () => pascalUtils.compile(sourceFile, executableFile)
+    it('Generates valid code', () => pascalUtils.compile(sourceFile, executableFile)
       .then(() => {
         expect(true).toBeTruthy();
       }));
-
-    it('Has correct output', async () => {
+    it('Generates correct code', () => {
       const commandToRun = process.platform === 'win32' ? executableFile : `./${executableFile}`;
       return exec(commandToRun)
         .then(({ stdout }) => {
           expect(stdout.trim()).toBe('Hello World!');
         });
     });
-
     afterAll(() => Promise.all([
-      fs.unlink(sourceFile), fs.unlink(executableFile), fs.unlink('test.o'),
+      fs.unlink(sourceFile),
+      fs.unlink(objectFile),
+      fs.unlink(executableFile),
     ]));
   });
 });
