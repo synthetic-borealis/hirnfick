d629729c0c0eb82ec1ea12fd825d4db9726221ca
Frazonar <snip.coloured@gmail.com>
2022-08-28 14:54:56 +0300

    test: rewrite Python tests

---
+++ __tests__/test.transpilers.python.js
@@ -0,0 +1,40 @@
+const asyncPyShell = require('../test-utils/async-python-shell');
+const {
+  WrongInputTypeError,
+  BracketMismatchError,
+  transpileToPython,
+} = require('..');
+
+const helloWorldCode = '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.';
+const bracketMismatchCode = '>>+++[[<-->]';
+const numberArray = [2, 4, 8, 16];
+
+function checkGeneratedCode(codeToCheck) {
+  it('Generates valid code', () => asyncPyShell.runString(codeToCheck)
+    .then(() => expect(true).toBeTruthy()));
+
+  it('Generates correct code', () => asyncPyShell.runString(codeToCheck)
+    .then((output) => {
+      expect(output).toEqual(
+        expect.arrayContaining(['Hello World!']),
+      );
+    }));
+}
+
+describe('Python transpiler', () => {
+  describe('Error handling', () => {
+    it('Throws WrongInputTypeError when given input of wrong type', () => {
+      // noinspection JSCheckFunctionSignatures
+      expect(() => transpileToPython(numberArray)).toThrow(WrongInputTypeError);
+    });
+    it('Throws BracketMismatchError when there\'s a bracket mismatch', () => {
+      expect(() => transpileToPython(bracketMismatchCode)).toThrow(BracketMismatchError);
+    });
+  });
+  describe('Code generation (dynamic array)', () => {
+    checkGeneratedCode(transpileToPython(helloWorldCode));
+  });
+  describe('Code generation (fixed array)', () => {
+    checkGeneratedCode(transpileToPython(helloWorldCode, false));
+  });
+});
