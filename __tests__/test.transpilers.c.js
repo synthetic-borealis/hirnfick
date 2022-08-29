55b9a7bcd6339ec4b282fd1eca294f1b492957d3
synthetic-borealis <snip.coloured@gmail.com>
2022-08-29 18:40:06 +0300

    test: add user input test for C transpiler

--- __tests__/test.transpilers.c.js
+++ __tests__/test.transpilers.c.js
@@ -1,18 +1,23 @@
 const cppUtils = require('cpp-utils');
 const fs = require('fs/promises');
-const process = require('node:process');
 const util = require('util');
+const childProcess = require('child_process');
 const {
   WrongInputTypeError,
   BracketMismatchError,
   transpileToC,
 } = require('../lib');
 
-const exec = util.promisify(require('child_process').exec);
+const exec = util.promisify(childProcess.exec);
 
 const helloWorldCode = '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.';
 const bracketMismatchCode = '>>+++[[<-->]';
+const userInputCode = ',.';
 const numberArray = [2, 4, 8, 16];
+const exeExtension = process.platform === 'win32' ? '.exe' : '';
+const executableFile = `test_c${exeExtension}`;
+const sourceFile = 'test_c.c';
+const commandToRun = process.platform === 'win32' ? executableFile : `./${executableFile}`;
 
 describe('C transpiler', () => {
   describe('Error handling', () => {
@@ -25,25 +30,56 @@ describe('C transpiler', () => {
     });
   });
   describe('Code generation', () => {
-    const outputCode = transpileToC(helloWorldCode);
-    const exeExtension = process.platform === 'win32' ? '.exe' : '';
-    const executableFile = `test_c${exeExtension}`;
-    const sourceFile = 'test_c.c';
-    beforeAll(() => fs.writeFile(sourceFile, outputCode));
-    it('Generates valid code', () => cppUtils.compileWithGcc(sourceFile, executableFile, true)
-      .then(() => {
-        expect(true).toBeTruthy();
-      }));
-    it('Generates correct code', () => {
-      const commandToRun = process.platform === 'win32' ? executableFile : `./${executableFile}`;
-      return exec(commandToRun)
-        .then(({ stdout }) => {
-          expect(stdout.trim()).toBe('Hello World!');
-        });
+    beforeAll(() => {
+      const outputCode = transpileToC(helloWorldCode);
+      return fs.writeFile(sourceFile, outputCode);
     });
     afterAll(() => Promise.all([
       fs.unlink(sourceFile),
       fs.unlink(executableFile),
     ]));
+    it('Generates valid code', () => cppUtils.compileWithGcc(sourceFile, executableFile, true)
+      .then(() => {
+        expect(true).toBeTruthy();
+      }));
+    it('Generates correct code', () => {
+
+      return exec(commandToRun)
+        .then(({ stdout }) => {
+          expect(stdout.trim()).toBe('Hello World!');
+        });
+    });
+  });
+  describe('Code generation (with user input)', () => {
+    beforeAll(() => {
+      const outputCode = transpileToC(userInputCode);
+      return fs.writeFile(sourceFile, outputCode);
+    });
+    // noinspection DuplicatedCode
+    afterAll(() => Promise.all([
+      fs.unlink(sourceFile),
+      fs.unlink(executableFile),
+    ]));
+    it('Generates valid code', () => cppUtils.compileWithGcc(sourceFile, executableFile, true)
+      .then(() => {
+        expect(true).toBeTruthy();
+      }));
+    it('Generates correct code', () => {
+      const inputChar = 'a';
+      const getPromise = () => new Promise((resolve, reject) => {
+        const child = childProcess.exec(`${commandToRun}`, (error, stdout) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(stdout.trim());
+        });
+        process.stdin.pipe(child.stdin);
+        process.stdin.push(`${inputChar}\n`);
+      });
+      return getPromise()
+        .then((out) => {
+          expect(out).toBe(inputChar);
+        });
+    });
   });
 });
