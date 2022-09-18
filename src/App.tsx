02d3d988bb5897db0ac483e5f4d2d4d3d0593446
synthetic-borealis <snip.coloured@gmail.com>
2022-09-18 23:55:23 +0300

    setup "under construction" temporary landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,15 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import githubIcon from './images/icons/icon-github.svg';
 import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React + Sass!</p>
-        <p>
-          <button type="button" onClick={() => setCount(() => count + 1)}>
-            count is:
-            {' '}
-            {count}
-          </button>
-        </p>
-        {count > 0 ? (
-          <p>
-            <code>
-              The count is now:
-              {' '}
-              {count}
-            </code>
-          </p>
-        ) : null}
-        <p>
-          Edit
-          {' '}
-          <code>App.tsx</code>
-          {' '}
-          and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <p className="App__subtitle">UNDER CONSTRUCTION</p>
+      <h1 className="App__title">HIRNFICK</h1>
+      <a className="App__link" href="https://github.com/synthetic-borealis/hirnfick.js/" target="_blank" rel="noreferrer">
+        <img className="App__link-icon" src={githubIcon} alt="GitHub icon" />
+      </a>
     </div>
   );
 }
