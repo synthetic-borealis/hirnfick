328867a37fb2ffd8b948de85da3b8848028d14c0
synthetic-borealis <snip.coloured@gmail.com>
2022-09-21 15:54:22 +0300

    build: generate deno friendly files

---
+++ deno/compilers/JavaScriptNode.ts
@@ -0,0 +1,60 @@
+import genIndent from '../utils/genIndent.ts';
+import compileToJsBase from './JavaScriptBase.ts';
+
+/**
+ * Converts a Brainfuck program to JavaScript (Node.js).
+ * @category Compilation
+ * @param {string} source Brainfuck source to convert.
+ * @param {boolean} isMemoryDynamic Enable dynamic memory array.
+ * @param {string} mainFunctionName Main function name.
+ * @param {number} indentSize Indentation size.
+ * @param {string} indentChar Indentation character.
+ * @returns {string} Generated JavaScript code.
+ * @throws {@link BracketMismatchError} if mismatching brackets are detected.
+ */
+export default function compileToJsNode(
+  source: string,
+  isMemoryDynamic = true,
+  mainFunctionName = 'main',
+  indentSize = 2,
+  indentChar = ' ',
+) {
+  const {
+    declaration: declarationLines,
+    definition: definitionLines,
+  } = compileToJsBase(
+    source,
+    isMemoryDynamic,
+    true,
+    indentSize,
+    indentChar,
+  );
+  const indent = genIndent(1, indentSize, indentChar);
+  const putchar = [
+    'function putchar() {',
+    `${indent}process.stdout.write(String.fromCharCode(cells[position]));`,
+    '}\n',
+  ];
+  const getchar = [
+    'function getchar() {',
+    `${indent}const buffer = Buffer.alloc(1);`,
+    `${indent}fs.readSync(process.stdin.fd, buffer, 0, 1);`,
+    `${indent}return buffer[0];`,
+    '}\n',
+  ];
+  const hasUserInput = source.indexOf(',') > -1;
+  const outputCodeArray: string[] = [];
+  if (hasUserInput) {
+    outputCodeArray.push('const fs = require(\'fs\');\n');
+  }
+  outputCodeArray.push(...declarationLines);
+  outputCodeArray.push(...putchar);
+  if (hasUserInput) {
+    outputCodeArray.push(...getchar);
+  }
+  outputCodeArray.push(`function ${mainFunctionName}() {`);
+  outputCodeArray.push(...(definitionLines.map((line) => `${indent}${line}`)));
+  outputCodeArray.push('}\n');
+  outputCodeArray.push(`${mainFunctionName}();`);
+  return outputCodeArray.join('\n');
+}
---
+++ deno/index.ts
@@ -0,0 +1,33 @@
+// Error Types
+import BracketMismatchError from './errors/bracketMismatch.ts';
+
+// Validation & Utilities
+import isValidProgram from './utils/isValidProgram.ts';
+
+// Compilation
+import compileToJsBase from './compilers/JavaScriptBase.ts';
+import compileToJsWeb from './compilers/JavaScriptWeb.ts';
+import compileToJsNode from './compilers/JavaScriptNode.ts';
+import compileToJsDeno from './compilers/JavaScriptDeno.ts';
+import compileToPython from './compilers/Python.ts';
+import compileToC from './compilers/C.ts';
+import compileToCpp from './compilers/CPP.ts';
+import compileToQBasic from './compilers/QBasic.ts';
+import compileToPascal from './compilers/Pascal.ts';
+import compileToKotlin from './compilers/Kotlin.ts';
+
+export {
+  BracketMismatchError,
+  isValidProgram,
+  compileToJsBase,
+  compileToJsWeb,
+  compileToJsNode,
+  compileToJsDeno,
+  compileToJsNode as compileToJsCli, // for backward compatibility
+  compileToPython,
+  compileToC,
+  compileToCpp,
+  compileToQBasic,
+  compileToPascal,
+  compileToKotlin,
+};
