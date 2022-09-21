04c4b1c807a9d27809878c890c12fe0520c3a15c
synthetic-borealis <snip.coloured@gmail.com>
2022-09-21 22:20:25 +0300

    feat: implement bf to rust compiler

---
+++ src/compilers/Rust.ts
@@ -0,0 +1,123 @@
+import BracketMismatchError from '../errors/bracketMismatch';
+import isValidProgram from '../utils/isValidProgram';
+import genIndent from '../utils/genIndent';
+import cleanCode from '../utils/cleanCode';
+
+/**
+ * Converts a Brainfuck program to Rust.
+ * @category Compilation
+ * @param {string} source Brainfuck source to convert.
+ * @param {number} indentSize Indentation size.
+ * @param {string} indentChar Indentation character.
+ * @returns {string} Generated C code.
+ * @throws {@link BracketMismatchError} if mismatching brackets are detected.
+ */
+export default function compileToRust(
+  source: string,
+  indentSize = 4,
+  indentChar = ' ',
+): string {
+  const cleanSource = cleanCode(source);
+  const sourceArray = Array.from(cleanSource);
+
+  if (!isValidProgram(cleanSource)) {
+    throw new BracketMismatchError();
+  }
+
+  let indent = genIndent(1, indentSize, indentChar);
+  const putChar = [
+    'fn put_char(c: char) {',
+    `${indent}let buf: [u8; 1] = [c as u8; 1];`,
+    `${indent}stdout().write(&buf).expect("Oh no :\\'(");`,
+    '}',
+  ];
+  const getChar = [
+    'fn get_char() -> char {',
+    `${indent}let mut buf: [u8; 1] = [0; 1];`,
+    `${indent}stdin().read_exact(&mut buf).expect("Oops :(");`,
+    `${indent}buf[0] as char`,
+    '}',
+  ];
+
+  const outputCodeArray = [
+    'use std::io::{Read, stdin, stdout, Write};',
+    '',
+    'fn main() {',
+    `${indent}let mut arr: [u8; 30000] = [0; 30000];`,
+    `${indent}let mut pos: i32 = 0;`,
+    '',
+  ];
+
+  let currentDepth = 0;
+
+  sourceArray.forEach((command) => {
+    indent = genIndent(currentDepth + 1, indentSize, indentChar);
+
+    switch (command) {
+      case '>':
+        outputCodeArray.push(`${indent}pos = if pos < 30000 {`);
+        indent += genIndent(1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}pos + 1`);
+        indent = genIndent(currentDepth + 1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}} else {`);
+        indent += genIndent(1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}pos`);
+        indent = genIndent(currentDepth + 1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}};`);
+        break;
+
+      case '<':
+        outputCodeArray.push(`${indent}pos = if pos > 0 {`);
+        indent += genIndent(1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}pos - 1`);
+        indent = genIndent(currentDepth + 1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}} else { pos };`);
+        break;
+
+      case '+':
+        outputCodeArray.push(`${indent}cells[pos] = if cells[pos] < 255 {`);
+        indent += genIndent(1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}cells[pos] + 1`);
+        indent = genIndent(currentDepth + 1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}} else { cells[pos] };`);
+        break;
+
+      case '-':
+        outputCodeArray.push(`${indent}cells[pos] = if cells[pos] > 0 {`);
+        indent += genIndent(1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}cells[pos] - 1`);
+        indent = genIndent(currentDepth + 1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}} else { cells[pos] };`);
+        break;
+
+      case '.':
+        outputCodeArray.push(`${indent}put_char(cells[pos] as char);`);
+        break;
+
+      case ',':
+        outputCodeArray.push(`${indent}cells[pos] = get_char() as u8;`);
+        break;
+
+      case '[':
+        outputCodeArray.push(`${indent}while cells[pos] > 0 {`);
+        currentDepth += 1;
+        break;
+
+      case ']':
+        currentDepth = Math.max(currentDepth - 1, 0);
+        indent = genIndent(currentDepth + 1, indentSize, indentChar);
+        outputCodeArray.push(`${indent}}`);
+        break;
+
+      // skip default case
+    }
+  });
+
+  outputCodeArray.push('}\n');
+  outputCodeArray.push(...putChar, '');
+  if (sourceArray.indexOf(',') >= 0) {
+    outputCodeArray.push(...getChar, '');
+  }
+
+  return outputCodeArray.join('\n');
+}
