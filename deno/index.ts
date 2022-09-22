8a37c20746f91f2a3d8ecfac0e211ddf14bc9f79
synthetic-borealis <snip.coloured@gmail.com>
2022-09-22 16:27:23 +0300

    generate updated deno files

--- deno/index.ts
+++ deno/index.ts
@@ -3,6 +3,7 @@ import BracketMismatchError from './errors/bracketMismatch.ts';
 
 // Validation & Utilities
 import isValidProgram from './utils/isValidProgram.ts';
+import cleanCode from './utils/cleanCode.ts';
 
 // Compilation
 import compileToJsBase from './compilers/JavaScriptBase.ts';
@@ -14,22 +15,20 @@ import compileToC from './compilers/C.ts';
 import compileToCpp from './compilers/CPP.ts';
 import compileToQBasic from './compilers/QBasic.ts';
 import compileToPascal from './compilers/Pascal.ts';
-import compileToKotlin from './compilers/Kotlin.ts';
 import compileToRust from './compilers/Rust.ts';
 
 export {
   BracketMismatchError,
   isValidProgram,
+  cleanCode,
   compileToJsBase,
   compileToJsWeb,
   compileToJsNode,
   compileToJsDeno,
-  compileToJsNode as compileToJsCli, // for backward compatibility
   compileToPython,
   compileToC,
   compileToCpp,
   compileToQBasic,
   compileToPascal,
-  compileToKotlin,
   compileToRust,
 };
