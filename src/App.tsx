4c85cb59eedec8561bd64ae075e453de5664961c
synthetic-borealis <snip.coloured@gmail.com>
2022-09-22 18:27:02 +0300

    optimise: use portal-storm to add elements to the head

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import Portal from 'portal-storm';
 import hirnfickLogo from './images/Hirnfick-logo.webp';
 import githubIcon from './images/icons/icon-github.svg';
 import './App.scss';
 
 function App() {
-  useEffect(() => {
-    // There's probably a better way to add a link to the document's head
-    const logoLink = document.createElement('link');
-    logoLink.rel = 'preload';
-    logoLink.as = 'image';
-    logoLink.href = hirnfickLogo;
-    logoLink.type = 'image/webp';
-    document.head.append(logoLink);
-  }, []);
   return (
     <div className="App">
       <p className="App__subtitle">UNDER CONSTRUCTION</p>
+      <Portal container={document.head}>
+        <link rel="preload" as="image" href={hirnfickLogo} type="image/webp" />
+      </Portal>
       <img className="App__logo" src={hirnfickLogo} width="384" height="384" alt="Hirnfick logo" />
       <h1 className="App__title">HIRNFICK</h1>
       <a className="App__link" href="https://github.com/synthetic-borealis/hirnfick.js/" target="_blank" rel="noreferrer">
