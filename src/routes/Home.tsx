ce10629a8d674d9b46674af2c3be7afe14766287
synthetic-borealis <snip.coloured@gmail.com>
2022-10-02 23:58:05 +0300

    upgrade dependencies & fix sizing issues

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,7 +5,7 @@ import hirnfickLogo from '../images/Hirnfick-logo.webp';
 function Home() {
   return (
     <section className="Home">
-      <img className="Home__logo" src={hirnfickLogo} width="384" height="384" alt="Hirnfick logo" />
+      <img className="Home__logo" src={hirnfickLogo} width="300" height="300" alt="Hirnfick logo" />
       <h1 className="Home__title">HIRNFICK</h1>
       <p className="Home__subtitle">
         A modern source-to-source Brainfuck compiler.
