85ab21f04eb940293a94e76f4f5f09caed37c75f
synthetic-borealis <snip.coloured@gmail.com>
2022-10-02 23:27:27 +0300

    feat: finish home route & navbar for desktop resolutions

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './NavBar.scss';
 
 function NavBar() {
+  const navLinkClass = ({ isActive }: { isActive: boolean }) => (
+    isActive ? 'NavBar__link NavBar__link_active' : 'NavBar__link'
+  );
   return (
     <header className="NavBar">
-      <p className="NavBar__placeholder-header-text">
-        Placeholder
-      </p>
+      <nav className="NavBar__link-container">
+        <NavLink className={navLinkClass} to="/">Home</NavLink>
+        <NavLink className={navLinkClass} to="/features">Features</NavLink>
+        <NavLink className={navLinkClass} to="/play">Play</NavLink>
+        <a
+          className="NavBar__link"
+          href="https://github.com/synthetic-borealis/hirnfick/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </nav>
     </header>
   );
 }
