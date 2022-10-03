8698efa343f1ff58f9a58f4db49d02d3341ca136
synthetic-borealis <snip.coloured@gmail.com>
2022-10-03 11:38:52 +0300

    add roboto mono and refactor layout

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './fonts/Roboto/Roboto.scss';
+import './fonts/RobotoMono/RobotoMono.scss';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import './Layout.scss';
 
 function Layout() {
   return (
-    <>
+    <div className="Layout">
       <NavBar />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
 
