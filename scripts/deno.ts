a1280b4861d72120991bb8a1d3f1d55d57525ea1
synthetic-borealis <snip.coloured@gmail.com>
2023-10-08 19:50:56 +0300

    build: port Deno converter to TypeScript

--- scripts/deno.ts
+++ scripts/deno.ts
@@ -1,11 +1,47 @@
-import { PythonShell } from 'python-shell';
 import * as path from 'path';
+import * as fs from 'fs';
+import * as fsPromises from 'fs/promises';
+
+function isDirectory(filePath: string): boolean {
+  const fileStats = fs.lstatSync(filePath);
+  return fileStats.isDirectory();
+}
+
+type ForTreePredicate = (file: string) => Promise<void>;
+
+async function forTree(basePath: string, predicate: ForTreePredicate): Promise<void> {
+  const files = await fsPromises.readdir(basePath);
+  const fileTransformers: Promise<void>[] = [];
+  const subdirectoryIterators: Promise<void>[] = [];
+  files.forEach((file) => {
+    const filePath = path.join(basePath, file);
+    if (isDirectory(filePath)) {
+      subdirectoryIterators.push(forTree(filePath, predicate));
+    } else {
+      fileTransformers.push(predicate(filePath));
+    }
+  });
+  await Promise.all([...subdirectoryIterators, ...fileTransformers]);
+}
 
 console.log('Generating Deno friendly TypeScript files');
-PythonShell.run(path.join(__dirname, 'deno.py'), undefined, (error) => {
-  if (error) {
-    console.error(error.message);
-  } else {
-    console.log('Success!');
+
+const SRC_PATH = './src';
+const DENO_PATH = './deno';
+const importLineEndRegex = /(?<=\S)';$/gm;
+
+try {
+  if (fs.existsSync(DENO_PATH) && isDirectory((DENO_PATH))) {
+    fs.rmSync(DENO_PATH, { recursive: true });
   }
-});
+  await fsPromises.cp(SRC_PATH, DENO_PATH, { recursive: true });
+  await forTree(DENO_PATH, async (filePath) => {
+    const fileContent = await fsPromises.readFile(filePath, 'utf8');
+    const transformedFileContent = fileContent.replace(importLineEndRegex, '.ts\';');
+    await fsPromises.writeFile(filePath, transformedFileContent);
+  });
+} catch (error) {
+  console.error(error);
+  process.exit(-1);
+}
+console.log('Success!');
